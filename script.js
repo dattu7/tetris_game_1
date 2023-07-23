8698a2075d3bee900d86e01d4dcb75299542fbc4
Gundumolu Dattatreya <126433896+dattu7@users.noreply.github.com>
2023-07-23 20:49:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,298 @@
+let canvas = document.querySelector("#tetris");
+let scoreboard = document.querySelector("#score");
+let ctx = canvas.getContext("2d");
+ctx.scale(30, 30);
+
+const SHAPES = [
+    [
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+    ],
+    [
+        [0, 1, 0],
+        [0, 1, 0],
+        [1, 1, 0],
+    ],
+    [
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 1],
+    ],
+    [
+        [1, 1, 0],
+        [0, 1, 1],
+        [0, 0, 0],
+    ],
+    [
+        [0, 1, 1],
+        [1, 1, 0],
+        [0, 0, 0],
+    ],
+    [
+        [1, 1, 1],
+        [0, 1, 0],
+        [0, 0, 0],
+    ],
+    [
+        [1, 1],
+        [1, 1],
+    ],
+];
+
+const COLORS = [
+    "#fff",
+    "#9b5fe0",
+    "#16a4d8",
+    "#60dbe8",
+    "#8bd346",
+    "#efdf48",
+    "#f9a52c",
+    "#d64e12",
+];
+
+const ROWS = 20;
+const COLS = 10;
+
+let grid = generateGrid();
+let fallingPieceObj = null;
+let score = 0;
+
+let gameStarted = false;
+let gamePaused = false;
+let intervalId;
+
+document.getElementById("startButton").addEventListener("click", startGame);
+document.getElementById("pauseButton").addEventListener("click", pauseGame);
+document.getElementById("restartButton").addEventListener("click", restartGame);
+
+function startGame() {
+    
+    if (!gameStarted) {
+        gameStarted = true;
+        document.getElementById('gameover').textContent = "";
+        document.getElementById('sr').textContent="";
+        intervalId = setInterval(newGameState, 500);
+    }
+}
+
+function pauseGame() {
+    if (gameStarted) {
+        if (!gamePaused) {
+            gamePaused = true;
+            clearInterval(intervalId);
+        } else {
+            gamePaused = false;
+            intervalId = setInterval(newGameState, 500);
+        }
+    }
+}
+
+function restartGame() {
+    grid = generateGrid();
+    score = 0;
+    fallingPieceObj = null;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    scoreboard.textContent = score;
+    // document.getElementById('gameover').textContent = "";
+    gameStarted = false;
+    gamePaused = false;
+    clearInterval(intervalId);
+    
+}
+function newGameState() {
+    checkGrid();
+    if (!fallingPieceObj && !gamePaused) {
+        fallingPieceObj = randomPieceObject();
+        renderPiece();
+    }
+    if (!gamePaused) {
+        moveDown();
+    }
+}
+
+function checkGrid() {
+    let count = 0;
+    for (let i = 0; i < grid.length; i++) {
+        let allFilled = true;
+        for (let j = 0; j < grid[0].length; j++) {
+            if (grid[i][j] == 0) {
+                allFilled = false;
+            }
+        }
+        if (allFilled) {
+            count++;
+            grid.splice(i, 1);
+            grid.unshift([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+        }
+    }
+    if (count == 1) {
+        score += 10;
+    } else if (count == 2) {
+        score += 30;
+    } else if (count == 3) {
+        score += 50;
+    } else if (count > 3) {
+        score += 100;
+    }
+    scoreboard.textContent = score;
+    
+}
+
+function generateGrid() {
+    let grid = [];
+    for (let i = 0; i < ROWS; i++) {
+        grid.push([]);
+        for (let j = 0; j < COLS; j++) {
+            grid[i].push(0);
+        }
+    }
+    return grid;
+}
+
+function randomPieceObject() {
+    let ran = Math.floor(Math.random() * 7);
+    let piece = SHAPES[ran];
+    let colorIndex = ran + 1;
+    let x = 4;
+    let y = 0;
+    return { piece, colorIndex, x, y };
+}
+
+function renderPiece() {
+    let piece = fallingPieceObj.piece;
+    for (let i = 0; i < piece.length; i++) {
+        for (let j = 0; j < piece[i].length; j++) {
+            if (piece[i][j] == 1) {
+                ctx.fillStyle = COLORS[fallingPieceObj.colorIndex];
+                ctx.fillRect(fallingPieceObj.x + j, fallingPieceObj.y + i, 1, 1);
+            }
+        }
+    }
+}
+
+function moveDown() {
+    if (!collision(fallingPieceObj.x, fallingPieceObj.y + 1)) fallingPieceObj.y += 1;
+    else {
+        let piece = fallingPieceObj.piece;
+        for (let i = 0; i < piece.length; i++) {
+            for (let j = 0; j < piece[i].length; j++) {
+                if (piece[i][j] == 1) {
+                    let p = fallingPieceObj.x + j;
+                    let q = fallingPieceObj.y + i;
+                    grid[q][p] = fallingPieceObj.colorIndex;
+                }
+            }
+        }
+        if (fallingPieceObj.y == 0) {
+            document.getElementById('gameover').textContent = "Game Over..!!";
+            document.getElementById('sr').textContent="Your Score Is:"+score;
+            restartGame();
+            
+        }
+       
+        fallingPieceObj = null;
+    }
+    renderGame();
+}
+
+function moveLeft() {
+    if (!collision(fallingPieceObj.x - 1, fallingPieceObj.y)) fallingPieceObj.x -= 1;
+    renderGame();
+}
+
+function moveRight() {
+    if (!collision(fallingPieceObj.x + 1, fallingPieceObj.y)) fallingPieceObj.x += 1;
+    renderGame();
+}
+
+function rotate() {
+    let rotatedPiece = [];
+    let piece = fallingPieceObj.piece;
+    for (let i = 0; i < piece.length; i++) {
+        rotatedPiece.push([]);
+        for (let j = 0; j < piece[i].length; j++) {
+            rotatedPiece[i].push(0);
+        }
+    }
+    for (let i = 0; i < piece.length; i++) {
+        for (let j = 0; j < piece[i].length; j++) {
+            rotatedPiece[i][j] = piece[j][i];
+        }
+    }
+
+    for (let i = 0; i < rotatedPiece.length; i++) {
+        rotatedPiece[i] = rotatedPiece[i].reverse();
+    }
+    if (!collision(fallingPieceObj.x, fallingPieceObj.y, rotatedPiece))
+        fallingPieceObj.piece = rotatedPiece;
+    renderGame();
+}
+
+function collision(x, y, rotatedPiece) {
+    let piece = rotatedPiece || fallingPieceObj.piece;
+    for (let i = 0; i < piece.length; i++) {
+        for (let j = 0; j < piece[i].length; j++) {
+            if (piece[i][j] == 1) {
+                let p = x + j;
+                let q = y + i;
+                if (p >= 0 && p < COLS && q >= 0 && q < ROWS) {
+                    if (grid[q][p] > 0) {
+                        return true;
+                    }
+                } else {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+function renderGame() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            ctx.fillStyle = COLORS[grid[i][j]];
+            ctx.fillRect(j, i, 1, 1);
+        }
+    }
+    renderPiece();
+}
+
+document.addEventListener("keydown", function (e) {
+    let key = e.key;
+    if (key === "ArrowDown") {
+        moveDown();
+    } else if (key === "ArrowLeft") {
+        moveLeft();
+    } else if (key === "ArrowRight") {
+        moveRight();
+    } else if (key === "ArrowUp") {
+        rotate();
+    }
+});
+
+function renderScore() {
+    scoreboard.textContent = score;
+}
+
+function updateScore() {
+    if (score >= 500) {
+        document.getElementById('gameover').textContent = "Congratulations..! You Have Won The Game";
+        restartGame();
+    }
+}
+
+function animate() {
+    if (gameStarted && !gamePaused) {
+        newGameState();
+        renderGame();
+        renderScore();
+        updateScore();
+    }
+    requestAnimationFrame(animate);
+}
+
+animate();
